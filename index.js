25fd5d6d6ec344ad2509ed5bae6fc927de819d12
justinzcruiz <justinzcruiz@gmail.com>
2024-10-03 01:10:02 +0300

    yaygit status first javascript code for dice 1 working! Success! DOM!

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-document
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+console.log("1st random number is " + randomNumber1);
+console.log("2nd random number is "+randomNumber2);
+var diceName = "images/dice" + randomNumber1 + ".png";
+document.querySelector("#dice1").setAttribute("src", diceName)
